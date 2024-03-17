af9c5d0dd383802b52f8c72ba037d37cdeb9d049
root <root@610-NgTech-Ansible-1.ngtech.home>
2024-03-17 20:52:52 +0200

    3

--- del-cidr-from-address-list.sh
+++ del-cidr-from-address-list.sh
@@ -16,7 +16,14 @@ then
 	exit 1
 fi
 
-
+function join_by {
+    local delimiter=${1-}
+    local format=${2-}
+    if shift 2; then
+        printf "%s" "$format" "${@/#/$delimiter}"
+    fi
+}
+IDS_TO_REMOVE=()
 
 if [ ! -z "${LIST_NAME}" ]
 then
@@ -36,21 +43,28 @@ then
 		VALUE_ID=$(curl -q -k -u ${USERNAME}:${PASSWORD} -X POST "https://${ROUTEROS_HOST}/rest/ip/firewall/address-list/print" \
 			-H "content-type: application/json" \
 			--data "{\".query\": [\"list=${LIST_NAME}\",\"address=${line}\"]}" | jq  -r '.[].".id"')
-#		echo "VALUE_ID is: ${VALUE_ID}"
+		echo "VALUE_ID is: ${VALUE_ID}"
 	if [ -z "${VALUE_ID}" ]
 	then
 		echo "There is no such address in the address list"
 		continue
 	else
-		DEL_RES=$(curl -q -k -u ${USERNAME}:${PASSWORD} -X DELETE "https://${ROUTEROS_HOST}/rest/ip/firewall/address-list/${VALUE_ID}")
-		if [ -z "${DEL_RES}" ]
-		then
-			echo "The address: ${line} was removed from list: ${LIST_NAME} successfulyy"
-		else
-			echo "There was an error removing the address: ${line} from list: ${LIST_NAME}}"
-			echo "${DEL_RES}"
-		fi
+		IDS_TO_REMOVE+=( "${VALUE_ID}" )
 	fi
 	done < ${LIST_FILENAME}
-fi
 
+	JOINED_IDS=$(join_by "," "${IDS_TO_REMOVE[@]}")
+
+
+	DEL_RES=$(curl -q -k -u ${USERNAME}:${PASSWORD} -X POST --url "https://${ROUTEROS_HOST}/rest/ip/firewall/address-list/remove" \
+		--header 'Content-Type: application/json' \
+		--data "{\".id\": \"${JOINED_IDS}\"}")
+
+	if [ "${DEL_RES}" == "[]" ]
+	then
+		echo "The address: ${JOINED_IDS} was removed from list: ${LIST_NAME} successfully"
+	else
+		echo "There was an error removing the address: ${line} from list: ${LIST_NAME}}"
+		echo "${DEL_RES}"
+	fi
+fi
